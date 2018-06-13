0783182553561d33d36670cea9a9e39a09ead09d
aroerick <a.roerick@gmail.com>
2018-06-13 10:38:18 -0700

    Start formatting front page php

--- themes/tamwood-theme/front-page.php
+++ themes/tamwood-theme/front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying the front page.
  *
  * @package RED_Starter_Theme
  */
@@ -9,15 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
+			<h1><?php echo CFS()->get( 'hero_title' ); ?></h1>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
