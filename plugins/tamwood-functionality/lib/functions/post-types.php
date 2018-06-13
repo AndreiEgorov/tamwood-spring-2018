8b4a45f60b5435908dacc84aa2b306912ced05f6
Andrei Egorov <andreyegorov1992@gmail.com>
2018-06-12 23:27:53 -0700

    Edit Campus Post Type

--- plugins/tamwood-functionality/lib/functions/post-types.php
+++ plugins/tamwood-functionality/lib/functions/post-types.php
@@ -7,67 +7,72 @@
 
 // Add your custom post types here...
 // Register Custom Post Type
-function tamwood_register_campus() {
+if ( ! function_exists('tamwood_register_campus') ) {
 
-	$labels = array(
-		'name'                  => _x( 'Campuses', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Campus', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Campuses', 'text_domain' ),
-		'name_admin_bar'        => __( 'Campus', 'text_domain' ),
-		'archives'              => __( 'Campus Archives', 'text_domain' ),
-		'attributes'            => __( 'Campus Attributes', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Campus:', 'text_domain' ),
-		'all_items'             => __( 'All Campuses', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Campus', 'text_domain' ),
-		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Campus', 'text_domain' ),
-		'edit_item'             => __( 'Edit Campus', 'text_domain' ),
-		'update_item'           => __( 'Update Campus', 'text_domain' ),
-		'view_item'             => __( 'View Campus', 'text_domain' ),
-		'view_items'            => __( 'View Campuses', 'text_domain' ),
-		'search_items'          => __( 'Search Campus', 'text_domain' ),
-		'not_found'             => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into Campus', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this Campus', 'text_domain' ),
-		'items_list'            => __( 'Campuses list', 'text_domain' ),
-		'items_list_navigation' => __( 'Campuses list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter Campuses list', 'text_domain' ),
-	);
-	$rewrite = array(
-		'slug'                  => 'campus',
-		'with_front'            => true,
-		'pages'                 => true,
-		'feeds'                 => true,
-	);
-	$args = array(
-		'label'                 => __( 'Campus', 'text_domain' ),
-		'description'           => __( 'Tamwood Campuses', 'text_domain' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'revisions', 'custom-fields', 'page-attributes' ),
-		'taxonomies'            => array( 'category', 'post_tag' ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-building',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => 'campus',
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'query_var'             => 'campus_post_type',
-		'rewrite'               => $rewrite,
-		'capability_type'       => 'post',
-		'show_in_rest'          => false,
-	);
-	register_post_type( 'post_type', $args );
-
-}
-add_action( 'init', 'tamwood_register_campus', 0 );
+    // Register Custom Post Type
+    function tamwood_register_campus() {
+    
+        $labels = array(
+            'name'                  => 'Campuses',
+            'singular_name'         => 'Campus',
+            'menu_name'             => 'Campuses',
+            'name_admin_bar'        => 'Campus',
+            'archives'              => 'Campus Archives',
+            'attributes'            => 'Campus Attributes',
+            'parent_item_colon'     => 'Parent Campus:',
+            'all_items'             => 'All Campuses',
+            'add_new_item'          => 'Add New Campus',
+            'add_new'               => 'Add New',
+            'new_item'              => 'New Campus',
+            'edit_item'             => 'Edit Campus',
+            'update_item'           => 'Update Campus',
+            'view_item'             => 'View Campus',
+            'view_items'            => 'View Campuses',
+            'search_items'          => 'Search Campus',
+            'not_found'             => 'Not found',
+            'not_found_in_trash'    => 'Not found in Trash',
+            'featured_image'        => 'Featured Image',
+            'set_featured_image'    => 'Set featured image',
+            'remove_featured_image' => 'Remove featured image',
+            'use_featured_image'    => 'Use as featured image',
+            'insert_into_item'      => 'Insert into Campus',
+            'uploaded_to_this_item' => 'Uploaded to this Campus',
+            'items_list'            => 'Campuses list',
+            'items_list_navigation' => 'Campuses list navigation',
+            'filter_items_list'     => 'Filter campuses list',
+        );
+        $rewrite = array(
+            'slug'                  => 'campus',
+            'with_front'            => true,
+            'pages'                 => true,
+            'feeds'                 => true,
+        );
+        $args = array(
+            'label'                 => 'Campus',
+            'description'           => 'Campus of Tamwood',
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'page-attributes' ),
+            'taxonomies'            => array( 'category', 'post_tag' ),
+            'hierarchical'          => false,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'menu_icon'             => 'dashicons-building',
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => 'campuses',
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'query_var'             => 'campus_post_type',
+            'rewrite'               => $rewrite,
+            'capability_type'       => 'post',
+            'show_in_rest'          => true,
+        );
+        register_post_type( 'campus', $args );
+    
+    }
+    add_action( 'init', 'tamwood_register_campus', 0 );
+    
+    }
