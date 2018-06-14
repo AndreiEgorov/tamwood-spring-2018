56116d18d39de531f8751c43b5258b5a9167e8a6
aroerick <a.roerick@gmail.com>
2018-06-14 11:39:54 -0700

    Enqueue font awesome, and footer menu.

--- themes/tamwood-theme/footer.php
+++ themes/tamwood-theme/footer.php
@@ -11,8 +11,10 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					
+				<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
 				</div><!-- .site-info -->
+				<div class="social-media">
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
