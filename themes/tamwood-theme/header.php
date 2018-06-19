2996bb6f24024be6d7381d6a4959ca520a748d2b
aroerick <a.roerick@gmail.com>
2018-06-19 16:55:32 -0700

    Finish header css.

--- themes/tamwood-theme/header.php
+++ themes/tamwood-theme/header.php
@@ -21,19 +21,23 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				</div><!-- .site-branding -->
-				<div class="header-container container">
-					<a class="hamburger menu-toggle mobile" type="button"></a>
-					<img class="desktop logo-desktop" src="<?php echo get_template_directory_uri() ?>/assets/logos/tamwood-logo.svg">
-					<nav id="site-navigation" class="main-navigation toggled" role="navigation">
-						<button class="mobile">Apply Now</button>	
-						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-					</nav><!-- #site-navigation -->
-					<img class="mobile logo-mobile" src="<?php echo get_template_directory_uri() ?>/assets/logos/LOGO@3x.png" width="55" height="55">
-					<img class="search-icon" src="<?php echo get_template_directory_uri() ?>/assets/icons/ic_search@3x.png" width="85" height="85">
-					<a class="desktop apply-now">Apply Now</a>
+				<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<div class="desktop-header">
+					<div class="logo-container">
+						<a href="<?php echo home_url() ?>">
+							<img class="desktop logo-desktop" src="<?php echo get_template_directory_uri() ?>/assets/logos/tamwood-logo.svg">
+						</a>
+					</div>
+					<div class="header-container">
+						<a class="hamburger menu-toggle mobile" type="button"></a>
+						<nav id="site-navigation" class="main-navigation toggled" role="navigation">
+							<button class="mobile apply-now">Apply Now</button>	
+							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						</nav><!-- #site-navigation -->
+						<img class="mobile logo-mobile" src="<?php echo get_template_directory_uri() ?>/assets/logos/LOGO@3x.png" width="55" height="55">
+						<img class="search-icon" src="<?php echo get_template_directory_uri() ?>/assets/icons/ic_search@3x.png" width="85" height="85">
+						<a class="desktop apply-now" href="<?php echo get_page_link( get_page_by_title( 'Apply Now' )->ID );?>">Apply Now</a>
+					</div>
 				</div>
 			</header><!-- #masthead -->
 
