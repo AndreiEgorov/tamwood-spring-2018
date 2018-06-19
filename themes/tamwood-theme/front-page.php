32ef65876fdbc00d4087c1b94b0ebfa2848ebb49
Effat Chaar <effat.shaar@gmail.com>
2018-06-19 14:21:36 -0700

    updated frontpage

--- themes/tamwood-theme/front-page.php
+++ themes/tamwood-theme/front-page.php
@@ -16,17 +16,25 @@ get_header(); ?>
 			<p><?php echo CFS()->get( 'page_copy' ); ?></p>
 
 			<section class="testimonials home-carousel">
+				<h1>Testimonials</h1>
 				<?php $loop = CFS()->get( 'testimonial' );
 				foreach ( $loop as $row ) {
 					?> 
 					<img src="<?php echo $row['testifier_image']; ?>">
-					
 					<p><?php echo $row['quote']; ?></p>
 					<p><?php echo $row['testifier']; ?></p>
 					<?php }?>
 			</section>
 			<section class="accreditations">
-
+				<h1>Accreditaions</h1>
+				<ul class="accreditations-logos">
+					<li>
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/BC_PrivateTrainingDesignated_RGB_Color Internet.png">
+					</li>
+					<li>
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/bc1@3x.png">
+					</li>
+				</ul>
 			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
