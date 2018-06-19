841122521ef8a9da79e067a307c370aaa1ada62d
Effat Chaar <effat.shaar@gmail.com>
2018-06-19 11:34:52 -0700

    adding modified style and font-page

--- themes/tamwood-theme/front-page.php
+++ themes/tamwood-theme/front-page.php
@@ -11,8 +11,8 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 		<?php get_template_part( 'template-parts/content','banner' )?>
 			<h1><?php echo CFS()->get( 'hero_title' ); ?></h1>
-			<h2><?php echo CFS()->get( 'page_title' ); ?></h2>
-			<h3><?php echo CFS()->get( 'blurb' ); ?></h3>
+			<h1><?php echo CFS()->get( 'page_title' ); ?></h2>
+			<h2><?php echo CFS()->get( 'blurb' ); ?></h3>
 			<p><?php echo CFS()->get( 'page_copy' ); ?></p>
 
 			<section class="testimonials home-carousel">
@@ -20,10 +20,10 @@ get_header(); ?>
 				foreach ( $loop as $row ) {
 					?> 
 					<img src="<?php echo $row['testifier_image']; ?>">
-					<?php
-					echo $row['quote'];
-					echo $row['testifier'];
-				}?>
+					
+					<p><?php echo $row['quote']; ?></p>
+					<p><?php echo $row['testifier']; ?></p>
+					<?php }?>
 			</section>
 			<section class="accreditations">
 
