c03577130939c9fdfcbd3e9f63e6c27355b10176
Effat Chaar <effat.shaar@gmail.com>
2018-06-19 11:21:11 -0700

    updated typography and accomodation page and fonts

--- themes/tamwood-theme/page-templates/accommodation.php
+++ themes/tamwood-theme/page-templates/accommodation.php
@@ -7,22 +7,22 @@ get_header(); ?>
     <?php $loop = CFS()->get( 'options' );
         foreach ( $loop as $row ) {
             ?> <h2> <?php echo $row['option_number']; ?></h2>
-            <h4> <?php echo $row['option_title']; ?> </h4>
+            <h3> <?php echo $row['option_title']; ?> </h3>
             <p><?php echo $row['option_blurb']; ?> </p> <?php
         }?>
 </section>
 <section>
-    <h3><?php    
+    <h2><?php    
         $transfer_props = CFS()->get_field_info( 'airport_transfer_service' );
         echo $transfer_props['label'];
-    ?></h3>
+    ?></h2>
     <p><?php echo CFS()->get( 'subsection_copy' ); ?></p>
     <?php $loop = CFS()->get( 'airport_transfer_service' );
         foreach ( $loop as $row ) { ?>
-            <h4> <?php echo $row['location']; ?> </h4>
+            <h3> <?php echo $row['location']; ?> </h3>
             <img src="<?php echo $row['location_icon']; ?>">
             <p> <?php echo $row['transfer_option']; ?> </p>
-            <h5> <?php echo $row['price']; ?> </h5> <?php
+            <h4> <?php echo $row['price']; ?> </h4> <?php
         }?>
 </section>
 <?php get_footer(); ?>
