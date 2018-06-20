7c5dea452a7617188d2ef6f016e6f1eff00ba97a
Andrei Egorov <andreyegorov1992@gmail.com>
2018-06-20 09:19:15 -0700

    Not accepting the change in content banner

--- themes/tamwood-theme/template-parts/content-banner.php
+++ themes/tamwood-theme/template-parts/content-banner.php
@@ -2,14 +2,8 @@
     <div class='hero-image'>    
     </div>
     
-<<<<<<< HEAD
     <h1><?php echo CFS()->get( 'page_title' ); ?></h1>
     
-=======
-    <div class='container'>
-
-    <h2><?php echo CFS()->get( 'page_title' ); ?></h2>
->>>>>>> d1c1aed8d34581a0f4df1d28cf6309c4b3bf1890
     <p><?php echo CFS()->get( 'page_copy' ); ?></p>
     
     </div>
