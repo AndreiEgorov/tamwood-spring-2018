819ee92c7402699932a5a9c59425e69b7cb29784
aroerick <a.roerick@gmail.com>
2018-06-22 14:33:58 -0700

    Complete sanitization of cfs requests.

--- themes/tamwood-theme/single-program.php
+++ themes/tamwood-theme/single-program.php
@@ -11,16 +11,16 @@ get_header(); ?>
 	<main id="main" class="site-main" role="main">
 	<?php get_template_part( 'template-parts/content','banner' )?>
 	<?php while ( have_posts() ) : the_post(); ?>
-
+	<!-- wp_kses( $row['option_blurb'], array( 'p' => array( 'class' => '' ) ) ); -->
 		<section class='program-section program-overview container'>
 			<h1><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></h1>
-			<h4 class='blurb'><?php echo CFS()->get( 'blurb' )?></h4>
-			<p class='overview-text'><?php echo CFS()->get( 'program_overview' )?></p>
+			<h4 class='blurb'><?php echo wp_kses((CFS()->get( 'blurb' )), array( 'p' => array( 'class' => '' ) ) );?></h4>
+			<p class='overview-text'><?php echo wp_kses((CFS()->get( 'program_overview' )),array( 'p' => array( 'class' => '' ) ) );?></p>
 			<?php $fields = CFS()->get( 'program_highlights_table' );?>
 			<ol type="1">
 				<?php 
 				foreach ( $fields as $field ) {?>
-					<li><?php echo $field['highlight']; ?></li>
+					<li><?php echo esc_html($field['highlight']); ?></li>
 				<?php 
 				}?>
 			</ol>
@@ -30,13 +30,13 @@ get_header(); ?>
 				foreach ( $fields as $field ) {?>
 					<li class='program-type'>
 						<div class='box-section'>
-							<h3 class='box-title'><?php echo $field['program_type']; ?></h3>
+							<h3 class='box-title'><?php echo esc_html($field['program_type']); ?></h3>
 							<div class='box-set'>
 								<div class='box'>
-									<?php echo $field['box_1']; ?>
+									<?php echo wp_kses( $field['box_1'], array( 'h4' => array( 'class' => '' ), 'p' => array( 'class' => '' ) ) ); ?>
 								</div>
 								<div class='box'>
-									<?php echo $field['box_2']; ?>
+									<?php echo wp_kses( $field['box_2'], array( 'h4' => array( 'class' => '' ), 'p' => array( 'class' => '' ) ) ); ?>
 								</div>
 							</div>		
 						</div>
@@ -67,11 +67,11 @@ get_header(); ?>
 		<section class="program-section details container">
 			<img src="<?php echo get_template_directory_uri() ?>/assets/icons/placeholder (1)@2x.png" alt="downtown building">
 			<h3>Program Length (REPLACE IMAGE)</h3>
-			<?php echo CFS()->get( 'program_length' )?>
+			<?php echo wp_kses(CFS()->get( 'program_length' ), array ( 'p' => array( 'class' => '' ) ) );?>
 		
 			<img src="<?php echo get_template_directory_uri() ?>/assets/icons/clock-150x150@2x.png" alt="downtown building">
 			<h3>Location (REPLACE IMAGE)</h3>
-			<?php echo CFS()->get( 'location' )?>
+			<?php echo wp_kses(CFS()->get( 'location' ), array ( 'p' => array( 'class' => '' ) ) ); ?>
 
 			<img src="<?php echo get_template_directory_uri() ?>/assets/icons/downtown.png" alt="downtown building">
 			<h3>Schedule (REPLACE IMAGE)</h3>
@@ -82,9 +82,9 @@ get_header(); ?>
 				foreach ( $schedule as $row ) {?>
 					<li>
 						<div>
-							<?php echo $row['column_1']; ?>
-							<?php echo $row['column_2']; ?>
-							<?php echo $row['column_3']; ?>	
+							<?php echo esc_html($row['column_1']); ?>
+							<?php echo esc_html($row['column_2']); ?>
+							<?php echo esc_html($row['column_3']); ?>	
 						</div>
 					</li>
 				<?php 
@@ -92,32 +92,21 @@ get_header(); ?>
 			</ul>
 		</section>
 
-		<section class='program-section certificates-and-hiring-partners container hidden'>
-			<button class="back-button">Back</button>
-			<h1 class="program-heading"><?php echo CFS()->get( 'certificates_box_title' ) ?></h1>
-			<?php 
-			$c_logos = CFS()->get( 'certificates_logos' );
-
-			foreach ( $c_logos as $c_logo ) {
-				echo '<img src=" ' . $c_logo['logo'] . '" alt="" />';
-			}?>
-		</section>
-
 		<section class="program-section courses container hidden">
 			<button class="back-button">Back</button>
-			<h1 class="program-heading"><?php echo CFS()->get( 'courses_page_title' )?></h1>
+			<h1 class="program-heading"><?php echo esc_html(CFS()->get( 'courses_page_title' ))?></h1>
 			<?php $course = CFS()->get( 'course' );?>
 			<ul>
 				<?php 
 				foreach ( $course as $row ) {?>
 					<li>
-						<?php echo $row['course_subject']; ?>
+						<?php echo esc_html($row['course_subject']); ?>
 					</li>
 					<li>
-						<?php echo $row['number_of_weeks']; ?>
+						<?php echo esc_html($row['number_of_weeks']); ?>
 					</li>
 					<li>
-						<?php echo $row['course_hours']; ?>	
+						<?php echo esc_html($row['course_hours']); ?>	
 					</li>
 				<?php 
 				}?>
@@ -128,8 +117,8 @@ get_header(); ?>
 				foreach ( $courses_text_block as $courses_element ) {?>
 					<li>
 						<div>
-							<?php echo $courses_element['text_block_title']; ?>
-							<?php echo $courses_element['text']; ?>	
+							<?php echo esc_html($courses_element['text_block_title']); ?>
+							<?php echo wp_kses($courses_element['text'], array ( 'p' => array( 'class' => '' ) ) ); ?>	
 						</div>
 					</li>
 				<?php 
@@ -139,49 +128,49 @@ get_header(); ?>
 
 		<section class="program-section requirements container hidden">
 			<button class="back-button">Back</button>
-			<h1 class="program-heading"><?php echo CFS()->get( 'requirements_page_title' )?></h1>
-			<p><?php echo CFS()->get( 'requirements' )?></p>
+			<h1 class="program-heading"><?php echo esc_html(CFS()->get( 'requirements_page_title' ))?></h1>
+			<?php echo wp_kses(CFS()->get( 'requirements' ), array( 'p' => array( 'class' => ''), 'ol' => array( 'class' => ''), 'li' => array( 'class' => '') ) )?>
 		</section>
 		
 		<section class='program-section start-dates container hidden'>
 			<button class="back-button">Back</button>
-			<h1 class="program-heading"><?php echo CFS()->get( 'start_dates_page_title' )?></h1>
+			<h1 class="program-heading"><?php echo esc_html(CFS()->get( 'start_dates_page_title' ))?></h1>
 			<div>
-				<h3><?php echo CFS()->get_field_info( 'summer' )['label'];?></h3>
-				<p><?php echo CFS()->get( 'summer' )?></p>
+				<h3><?php echo esc_html(CFS()->get_field_info( 'summer' )['label']);?></h3>
+				<p><?php echo  wp_kses(CFS()->get( 'summer' ) , array( 'p' => array( 'class' => ''), 'ol' => array( 'class' => ''), 'li' => array( 'class' => '') ))?></p>
 			</div>
 			<div>
-				<h3><?php echo CFS()->get_field_info( 'fall' )['label'];?></h3>
-				<p><?php echo CFS()->get( 'fall' )?></p>
+				<h3><?php echo esc_html(CFS()->get_field_info( 'fall' )['label']);?></h3>
+				<p><?php echo  wp_kses(CFS()->get( 'fall' ), array( 'p' => array( 'class' => ''), 'ol' => array( 'class' => ''), 'li' => array( 'class' => '') ))?></p>
 			</div>
 			<div>
-				<h3><?php echo CFS()->get_field_info( 'winter' )['label'];?></h3>
-				<p><?php echo CFS()->get( 'winter' )?></p>
+				<h3><?php echo esc_html(CFS()->get_field_info( 'winter' )['label']);?></h3>
+				<p><?php echo  wp_kses(CFS()->get( 'winter' ), array( 'p' => array( 'class' => ''), 'ol' => array( 'class' => ''), 'li' => array( 'class' => '') ))?></p>
 			</div>
 			<div>
-				<h3><?php echo CFS()->get_field_info( 'spring' )['label'];?></h3>
-				<p><?php echo CFS()->get( 'spring' )?></p>
+				<h3><?php echo esc_html(CFS()->get_field_info( 'spring' )['label']);?></h3>
+				<p><?php echo wp_kses(CFS()->get( 'spring' ), array( 'p' => array( 'class' => ''), 'ol' => array( 'class' => ''), 'li' => array( 'class' => '') ))?></p>
 			</div>
 		</section>
 
 		<section class='program-section program-fees container hidden'>
 			<button class="back-button">Back</button>
-			<h1 class="program-heading"><?php echo CFS()->get( 'tuition_page_title' )?></h1>
+			<h1 class="program-heading"><?php echo esc_html(CFS()->get( 'tuition_page_title' ))?></h1>
 			<?php $program_fees_box1 = CFS()->get( 'program_fees_box' );?>
 			<ul>
 				<?php 
 				foreach ( $program_fees_box1 as $element ) {?>	
 					<li>
 						<div>
-							<h2> <?php echo $element['box_title']; ?></h2>
-							<p><?php echo $element['blurb']; ?></p>
+							<h2> <?php echo esc_html($element['box_title']); ?></h2>
+							<?php echo wp_kses($element['blurb'], array( 'p' => array( 'class' => '' ) ) ); ?>
 							<ul>	
 								<?php 		
 								$inner_loop = $element['fees_box'];	
 
 								foreach($inner_loop as $value){ ?>
-									<li> <?php echo $value['type_of_fee']; ?></li>
-									<li> <?php echo $value['fee_amount']; ?></li>
+									<li> <?php echo esc_html($value['type_of_fee']); ?></li>
+									<li> <?php echo esc_html($value['fee_amount']); ?></li>
 								<?php 
 								} ?>
 							</ul>
@@ -190,6 +179,17 @@ get_header(); ?>
 				<?php 
 				}?>
 			</ul>
+		</section>
+
+		<section class='program-section certificates-and-hiring-partners container hidden'>
+			<button class="back-button">Back</button>
+			<h1 class="program-heading"><?php echo esc_html(CFS()->get( 'certificates_box_title' )) ?></h1>
+			<?php 
+			$c_logos = CFS()->get( 'certificates_logos' );
+
+			foreach ( $c_logos as $c_logo ) {
+				echo '<img src=" ' . esc_url($c_logo['logo']) . '" alt="" />';
+			}?>
 		</section>
 	<?php endwhile; // End of the loop. ?>
 	</main><!-- #main -->
