42fb22d2bd2f67331b9678bbbd6beff90e10b964
Andrei Egorov <andreyegorov1992@gmail.com>
2018-06-25 07:34:37 -0700

    Style campus pages mobile

--- themes/tamwood-theme/single-campus.php
+++ themes/tamwood-theme/single-campus.php
@@ -11,35 +11,46 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
         <?php get_template_part( 'template-parts/content','banner' )?>
 
-        <?php echo esc_html(CFS()->get( 'article_intro' ))?>
-        <?php echo esc_html(CFS()->get( 'main_text' ))?>
-        
-        <a class="primary-main" href="#">Apply Now</a>
+        <div class='container narrow'>
 
+            <h2 class='intro'><?php echo wp_kses( CFS()->get( 'article_intro' ), array('p'));?></h2>
+           <p>  <?php echo wp_kses( CFS()->get( 'main_text' ), array('p'));?> </p> 
+            <div class='button-box'>
+            <a class="apply-button-green" href="#">Apply Now</a>
+            </div>
+        
+        </div>
+
+       
+        <section class='campus-features'>
+
+       
         <?php 
         
         $fields = CFS()->get( 'feature_logo_and_text' );
         foreach ( $fields as $field ) {?>
            
+            <div class='feature'>
+
             
             <?php
             echo '<img src="' . esc_url($field['feature_icon']) . '"/>';
+            ?>
+           <p class='feature-description'>
+            <?php
             echo $field['feature_description'];
+            ?> 
+            </p>
+            
+            </div><?php
         }
         
         ?>
-
-        <!-- This entire block will need to be deleted once the code is deployed, Andrei -->
-        <img src="<?php echo get_template_directory_uri() ?>/assets/icons/downtown.png" alt="downtown building">
-        <p>Centrally located in downtown Vancover</p>
-        <img src="<?php echo get_template_directory_uri()?>/assets/icons/nightlife.png" alt="nightlive image">
-        <p>Walking distance to shopping and nightlife</p>
-        <img src="<?php echo get_template_directory_uri()?>/assets/icons/Wifi.png" alt="Wifi image">
-        <p>25 student computers and free wifi</p>
-        <!-- --------------------------------- -->
+        </section>
+       
        
         <div>
-            <?php echo esc_html(CFS()->get( 'campus_location' ))?>
+            <?php echo esc_html( CFS()->get( 'campus_location' ), array('p'));?>
             <p><?php echo esc_html(CFS()->get('contact_info_title'))?></p>
             <div><?php echo esc_html(CFS()->get('contact_info'))?></div>
         </div>
