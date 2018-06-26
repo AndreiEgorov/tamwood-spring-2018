6c9adbdacfd93fd153cbdae533a1722aea3ac9dd
Effat Chaar <effat.shaar@gmail.com>
2018-06-26 00:11:17 -0700

    styling employment-service page

--- themes/tamwood-theme/page-employment_services.php
+++ themes/tamwood-theme/page-employment_services.php
@@ -1,26 +1,31 @@
 <?php 
 get_header();
 get_template_part( 'template-parts/content', 'banner' ); ?>
-<section>
-    <h3><?php echo esc_html(CFS()->get( 'section_title' )); ?></h3>
+<section class="section n-1">
+    <h3 class="section-title"><?php echo esc_html(CFS()->get( 'section_title' )); ?></h3>
     <h4><?php echo esc_html(CFS()->get( 'section_info' )); ?></h4>
-    <ul>
+    <ul class="info-list">
         <?php $loop = CFS()->get( 'list' );
         foreach ( $loop as $row ) { ?>
-            <li><?php echo esc_html($row['bullet_item']);?></li> <?php
+            <li><i class="fa fa-check"></i><?php echo esc_html($row['bullet_item']);?></li> <?php
         }?>
     </ul>
-    <p class='footnote'><?php echo esc_html(CFS()->get( 'footnote' )); ?></p>
+    <p class="footnote"><?php echo esc_html(CFS()->get( 'footnote' )); ?></p>
     <?php echo wp_kses( CFS()->get( 'section_copy' ), array( 'p' => array( 'class' => '' ) ) );?>
 </section>
-<section>
+<section class="section n-2">
     <?php $loop = CFS()->get( 'info_section' );
         foreach ( $loop as $row ) { ?>
-            <div>
+            <div class="info-section-item">
                 <h4><?php echo esc_html($row['info_title']);?></h4>
-                <img src="<?php echo esc_url($row['info_image']); ?>">
-                <?php echo wp_kses( $row['info_copy'], array( 'p' => array( 'class' => '' ) ) );?>
-            </div><?php
-    }?>
+                <div class="info-img">
+                    <img src="<?php echo esc_url($row['info_image']); ?>">
+                </div>
+                <p><?php echo wp_kses( $row['info_copy'], array( 'p' => array( 'class' => '' ) ) );?></p>      
+            </div> <!--info-section-item-->
+        <?php }?>
 </section>
+<div class="apply-now-button">
+    <a class='apply-button' href="<?php echo get_page_link( get_page_by_title( 'Apply Now' )->ID );?>">Apply Now</a>
+</div>
 <?php get_footer(); ?>
